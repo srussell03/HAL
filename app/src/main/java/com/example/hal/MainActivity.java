81edbce3d7ba21f56c389088f616b184fb3b50d8
Seth Russell <seth.ru4116@vusd.us>
2020-08-28 10:51:58 -0700

    Initial commit

---
+++ app/src/main/java/com/example/hal/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.hal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+    private String answer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        answer.getAnswer();
+    }
+    public String getAnswer(){
+        answer = 
+    }
+}
